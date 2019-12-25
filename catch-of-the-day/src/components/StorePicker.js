1286b45e26507d19c89bf031a8fa2a3a88bc74e0
Mamady Sillah <supurpisces310@msn.com>
2019-12-24 19:23:00 -0500

    created store picker.js

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+    render(){
+        return <form className="Store-selector">
+         <h2>Please Enter a Store</h2>   
+        </form>
+    }
+}
+
+export default StorePicker;
